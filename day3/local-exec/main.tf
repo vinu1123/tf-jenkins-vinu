4fe0675dd3613608d9457ac070471ef93e40e4cb
arjunachari12 <68580129+arjunachari12@users.noreply.github.com>
2025-03-05 17:24:58 +0530

    Create main.tf

---
+++ day3/local-exec/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-09e143e99e8fa74f9"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "arjun"
+  }
+
+  # local-exec provisioner
+  provisioner "local-exec" {
+    command = "echo Instance ${self.id} with IP ${self.public_ip} created successfully >> instance.log"
+  }
+}
