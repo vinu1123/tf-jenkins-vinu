91398c688fbf024ae87d177d0a2261581da469b5
arjunachari12 <68580129+arjunachari12@users.noreply.github.com>
2025-03-06 16:44:01 +0530

    Create variables.tf

---
+++ day4/terraform-modules-rds-s3/modules/rds/variables.tf
@@ -0,0 +1,38 @@
+variable "db_name" {
+  description = "Database name"
+  type        = string
+}
+
+variable "username" {
+  description = "Master username"
+  type        = string
+}
+
+variable "password" {
+  description = "Master password"
+  type        = string
+}
+
+variable "storage" {
+  description = "Allocated storage size"
+  type        = number
+  default     = 20
+}
+
+variable "engine" {
+  description = "Database engine"
+  type        = string
+  default     = "mysql"
+}
+
+variable "engine_version" {
+  description = "Database engine version"
+  type        = string
+  default     = "8.0"
+}
+
+variable "instance_class" {
+  description = "Database instance type"
+  type        = string
+  default     = "db.t3.micro"
+}
