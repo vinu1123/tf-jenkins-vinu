f1cba92b6e211818a4d3fe1ef818762963e4d5ee
arjunachari12 <68580129+arjunachari12@users.noreply.github.com>
2025-03-06 13:01:34 +0530

    Create main.tf

---
+++ day4/terraform-with-modules/modules/security-group/main.tf
@@ -0,0 +1,34 @@
+resource "aws_security_group" "web_sg" {
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-security-group"
+  }
+}
+
+variable "vpc_id" {}
+
+output "security_group_id" {
+  value = aws_security_group.web_sg.id
+}
